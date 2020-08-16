0ba3448eb2d2f2b28d7ce42d3dc120533c2e0fb8
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-08-16 17:07:37 +0900

    追加ボタンをReduxで実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { useSelector, useDispatch } from 'react-redux'
 import produce from 'immer'
-import { randomID, sortBy, reorderPatch } from './util'
+import { randomID, reorderPatch } from './util'
 import { api, ColumnID, CardID } from './api'
 import { Header as _Header } from './Header'
 import { Column } from './Column'
 import { DeleteDialog } from './DeleteDialog'
 import { Overlay as _Overlay } from './Overlay'
-
-type State = {
-  columns?: {
-    id: ColumnID
-    title?: string
-    text?: string
-    cards?: {
-      id: CardID
-      text?: string
-    }[]
-  }[]
-  cardsOrder: Record<string, CardID | ColumnID | null>
-}
 
 export function App() {
   const dispatch = useDispatch()
@@ -35,8 +22,6 @@ export function App() {
 
   const columns = useSelector(state => state.columns)
   const cardsOrder = useSelector(state => state.cardsOrder)
-  // TODO ビルドを通すためだけのスタブ実装なので、ちゃんとしたものにする
-  const setData = fn => fn({ cardsOrder: {} })
 
   const cardIsBeingDeleted = useSelector(state => Boolean(state.deletingCardID))
   const setDeletingCardID = (cardID: CardID) =>
@@ -77,44 +62,39 @@ export function App() {
     })()
   }, [dispatch])
 
-  const [draggingCardID, setDraggingCardID] = useState<CardID | undefined>(
-    undefined,
-  )
+  const draggingCardID = useSelector(state => state.draggingCardID)
+  const setDraggingCardID = (cardID: CardID) =>
+    dispatch({
+      type: 'Card.StartDragging',
+      payload: {
+        cardID,
+      },
+    })
 
   const dropCardTo = (toID: CardID | ColumnID) => {
     const fromID = draggingCardID
     if (!fromID) return
 
-    setDraggingCardID(undefined)
-
     if (fromID === toID) return
 
     const patch = reorderPatch(cardsOrder, fromID, toID)
-
-    setData(
-      produce((draft: State) => {
-        draft.cardsOrder = {
-          ...draft.cardsOrder,
-          ...patch,
-        }
-
-        const unorderedCards = draft.columns?.flatMap(c => c.cards ?? []) ?? []
-        draft.columns?.forEach(column => {
-          column.cards = sortBy(unorderedCards, draft.cardsOrder, column.id)
-        })
-      }),
-    )
+    dispatch({
+      type: 'Card.Drop',
+      payload: {
+        toID,
+      },
+    })
     api('PATCH /v1/cardsOrder', patch)
   }
 
   const setText = (columnID: ColumnID, value: string) => {
-    setData(
-      produce((draft: State) => {
-        const column = draft.columns?.find(c => c.id === columnID)
-        if (!column) return
-        column.text = value
-      }),
-    )
+    dispatch({
+      type: 'InputForm.SetText',
+      payload: {
+        columnID,
+        value,
+      },
+    })
   }
 
   const addCard = (columnID: ColumnID) => {
@@ -125,23 +105,14 @@ export function App() {
     const cardID = randomID() as CardID
 
     const patch = reorderPatch(cardsOrder, cardID, cardsOrder[columnID])
+    dispatch({
+      type: 'InputForm.ConfirmInput',
+      payload: {
+        columnID,
+        cardID,
+      },
+    })
 
-    setData(
-      produce((draft: State) => {
-        const column = draft.columns?.find(c => c.id === columnID)
-        if (!column?.cards) return
-
-        column.cards.unshift({
-          id: cardID,
-          text: column.text,
-        })
-        column.text = ''
-        draft.cardsOrder = {
-          ...draft.cardsOrder,
-          ...patch,
-        }
-      }),
-    )
     api('POST /v1/cards', {
       id: cardID,
       text,
